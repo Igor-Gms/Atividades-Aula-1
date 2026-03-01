2371a4a8b2f3905c7f6f226fc854aa61641f545e
Igor Gomes <igor.barbosa.tads@gmail.com>
2026-03-01 17:58:14 -0300

    Update Exercicio5.py

--- Exercicio5.py
+++ Exercicio5.py
@@ -3,7 +3,7 @@ print("=== Verificador de Ano Bissexto ===")
 ano = int(input("Digite o Ano Para Verificar Se Ele É Bissexto: "))
 
 if (ano % 4 == 0 and  ano % 100 !=0) or (ano % 400 == 0 ):
-    print(f"O ano {ano} é Bissexto!!!")
+    print(f"O Ano {ano} é Bissexto!!!")
     
 else:
-    print(f"O ano {ano} Nao é Bissexto!!!")
+    print(f"O Ano {ano} Não é Bissexto!!!")
