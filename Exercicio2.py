ce07f114acd7640707a5ec8c9e4b7ac19c2bcd31
Igor Gomes <igor.barbosa.tads@gmail.com>
2026-03-03 22:53:47 -0300

    Atualizar o Exercicio2.py

--- Exercicio2.py
+++ Exercicio2.py
@@ -5,7 +5,7 @@ nota2 = float(input("Digite a Segunda Nota: "))
 
 média = (nota1+nota2)/2
 
-if média >5<6.9:
+if média <6.9 >5:
     print(f"Nota => {média:.1f} Classificaçäo: Recuperaçäo!!!!")
 
 elif média >=7:
