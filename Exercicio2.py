4c942a8e5f0cc74bddcaae315252a8ef293d6cbe
Igor Gomes <igor.barbosa.tads@gmail.com>
2026-03-04 21:34:03 -0400

    Refactor grading logic and output format

--- Exercicio2.py
+++ Exercicio2.py
@@ -5,11 +5,11 @@ nota2 = float(input("Digite a Segunda Nota: "))
 
 média = (nota1+nota2)/2
 
-if média <6.9 >5:
+if média  <5:
+    print(f"Nota => {média:.1f} Classificaçäo: Reprovado!!!")
+
+elif média <6.9 >5:
     print(f"Nota => {média:.1f} Classificaçäo: Recuperaçäo!!!!")
 
-elif média >=7:
-    print(f"Nota => {média:.1f} Classificaçäo: Aprovado!!!")
-
-elif média <5:
-    print(f"Nota => {média:.1f} Classificaçäo: Reprovado!!!")
+else:
+        print(f"Nota => {média:.1f} Classificaçäo: Aprovado!!!")
